2ef6560f5fa9142d515b594b8d6b66987e9a4a94
Erick Souza <erick.souza9@senaisp.edu.br>
2021-12-17 17:41:45 -0300

    working... 6.5

--- spmgMobile/App.js
+++ spmgMobile/App.js
@@ -1,112 +1,137 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
+  Image,
+  ImageBackground,
+  TextInput,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import api from './src/services/api';
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      passwd: '',
+    };
+  }
+  doLogin = async () => {
+    console.warn(this.state.email + ' ' + this.state.passwd);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    const response = await api.post('/login', {
+      email: this.state.email,
+      passwd: this.state.passwd,
+    });
+    const token = response.data.token;
+    await AsyncStorage.setItem('userToken', token);
+    // if (resposta.status == 200) {
+    //   this.props.navigation.navigate('Agendamento');
+    // }
+
+    console.warn(token);
+
+    //
   };
 
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+  render() {
+    return (
+      <ImageBackground
+        source={require('./assets/img/background-login.png')}
+        style={StyleSheet.absoluteFillObject}>
+        {/* retangulo roxo */}
+        <View style={styles.overlay} />
+        <View style={styles.main}>
+          {/* <Image
+            source={require('../../assets/img/loginIcon2x.png')}
+            style={styles.mainImgLogin}
+          /> */}
+
+          <TextInput
+            style={styles.inputLogin}
+            placeholder="username"
+            placeholderTextColor="#FFF"
+            keyboardType="email-address"
+            // ENVENTO PARA FAZERMOS ALGO ENQUANTO O TEXTO MUDA
+            onChangeText={email => this.setState({email})}
+          />
+
+          <TextInput
+            style={styles.inputLogin}
+            placeholder="password"
+            placeholderTextColor="#FFF"
+            keyboardType="default" //para default nao obrigatorio.
+            secureTextEntry={true} //proteje a senha.
+            // ENVENTO PARA FAZERMOS ALGO ENQUANTO O TEXTO MUDA
+            onChangeText={passwd => this.setState({passwd})}
+          />
+
+          <TouchableOpacity
+            style={styles.btnLogin}
+            onPress={this.doLogin}>
+            <Text style={styles.btnLoginText}>Login</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+      // </ImageBackground>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+    //antes da main
+    overlay: {
+      ...StyleSheet.absoluteFillObject, //todas as prop do styleShhet, e vamos aplica o abosluteFIL...
+      backgroundColor: 'rgba(183,39,255,0.79)', //rgba pq vamos trabalhar com transparencia.
+    },
+  
+    // conteúdo da main
+    main: {
+      // flex: 1,
+      //backgroundColor: '#F1F1F1', retirar pra nao ter conflito.
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '100%',
+    },
+  
+    mainImgLogin: {
+      tintColor: '#FFF', //confirmar que sera branco
+      height: 100, //altura
+      width: 90, //largura img nao é quadrada
+      margin: 60, //espacamento em todos os lados,menos pra cima.
+      marginTop: 0, // tira espacamento pra cima
+    },
+  
+    inputLogin: {
+      width: 240, //largura mesma do botao
+      marginBottom: 40, //espacamento pra baixo
+      fontSize: 18,
+      color: '#FFF',
+      borderBottomColor: '#FFF', //linha separadora
+      borderBottomWidth: 2, //espessura.
+    },
+  
+    btnLoginText: {
+      fontSize: 12, //aumentar um pouco
+      fontFamily: 'Open Sans Light', //troca de fonte
+      color: '#B727FF', //mesma cor identidade
+      letterSpacing: 6, //espacamento entre as letras
+      textTransform: 'uppercase', //estilo maiusculo
+    },
+    btnLogin: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      height: 38,
+      width: 240,
+      backgroundColor: '#FFFFFF',
+      borderColor: '#FFFFFF',
+      borderWidth: 1,
+      borderRadius: 4,
+      shadowOffset: {height: 1, width: 1},
+    },
 });
-
-export default App;
